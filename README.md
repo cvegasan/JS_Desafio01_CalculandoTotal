c050bb47e779521cb9bb401f39f0066013a0ad35
Cristian Alberto Vega Sanhueza <Cristian.Vega@sonda.com>
2024-06-02 00:50:34 -0400

    Proyecto inicial

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS_Desafio01_CalculandoTotal
+## Deploy: https://cvegasan.github.io/Desafio05_Animaciones/
---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+// REQUERIMIENTOS
+// 1. Agrega el evento de JavaScript que reacciona ante un clic en el elemento HTML que
+//    corresponde.
+//    (2.5 puntos)
+// 2. Aplicar los querySelector() e innerHTML() de manera correcta para modificar los
+//    elementos del DOM.
+//    (2.5 puntos)
+// 3. Implementa la lógica para modificar total a pagar en función de la cantidad.
+//    (2.5 puntos)
+// 4. Hacer funcionar los botones de + y - para aumentar y disminuir la cantidad de
+//    productos.
+//    (2.5 puntos)
+
+//Declaración de constantes
+const precio = 400000; //valor por producto
+
+//Declaración de variables
+let precioSpan = document.querySelector(".precio-inicial"); //selector por clase, por id se llama getElementById
+precioSpan.innerHTML = precio;
+
+let valortotalSpan = document.querySelector(".valor-total");//selector por clase
+
+let pedido  = 0;
+let cantidad= 0;
+
+function fnactualizatotal(pedido,precio){
+    valortotalSpan.innerHTML = pedido * precio;
+    return 0;
+}
+
+//Declaración de funciones
+function fnsumaruno() {
+    pedido += 1;
+    cantidad = document.querySelector(".cantidad");
+    cantidad.innerHTML = pedido;
+    fnactualizatotal(pedido,precio);
+    return 0;
+}
+function fnrestaruno() {
+    if (pedido > 0) {
+        pedido -= 1;
+    }
+    cantidad = document.querySelector(".cantidad");
+    cantidad.innerHTML = pedido;
+        fnactualizatotal(pedido,precio);
+    return 0;
+}
